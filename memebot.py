0a680dd28a7b17bb97126046e38ab5277c7abf25
Lennon Donaghy <donaghylennon@gmail.com>
2021-01-05 17:11:30 +0000

    Changed the send_memes task so that it waits until a specified start time before retrieving the memes, and then waits for a specified interval between each meme it sends. Also removed commented out tutorial code.

--- memebot.py
+++ memebot.py
@@ -1,10 +1,14 @@
-#!/usr/bin/env python3 
-import os 
-import discord 
-from discord.ext import commands, tasks 
-import praw 
+#!/usr/bin/env python3
+import os
+import discord
+from discord.ext import commands, tasks
+import praw
+import asyncio
+import datetime
 
 SUBREDDIT = 'dankmemes'
+START_TIME = datetime.timedelta(hours=10)
+INTERVAL = datetime.timedelta(hours=2).total_seconds()
 
 CHANNEL_ID = os.environ.get('MY_PRIV_CHANNEL_ID')
 DISCORD_CLIENT_ID = os.environ.get('DISCORD_CLIENT_ID')
@@ -16,37 +20,21 @@ REDDIT_USER_AGENT = os.environ.get('REDDIT_USER_AGENT')
 
 client = discord.Client()
 
+
 @client.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
 
 
-# @client.event
-# async def on_message(message):
-#     if message.author == client.user:
-#         return
-
-#     if message.content.startswith('$hello'):
-#         await message.channel.send('Hello!')
-
-#     await send_message()
-
-
-# @tasks.loop(minutes=1)
-# async def once_per_minute():
-#     channel = client.get_channel(CHANNEL_ID)
-#     print(f"Got channel {channel}")
-#     await channel.send("Minute message")
-
-
-# @once_per_minute.before_loop
-# async def before_once_per_minute():
-#     await client.wait_until_ready()
-#     print("Finished waiting")
-
-
 @tasks.loop(hours=24)
 async def send_memes():
+    current_time = datetime.datetime.now().time()
+    delta_now = datetime.timedelta(
+        hours=current_time.hour, minutes=current_time.minute, seconds=current_time.second)
+    if delta_now <= START_TIME:
+        await asyncio.sleep((START_TIME - delta_now).total_seconds())
+    else:
+        await asyncio.sleep(((START_TIME + datetime.timedelta(days=1)) - delta_now).total_seconds())
     channel = client.get_channel(CHANNEL_ID)
     reddit = praw.Reddit(client_id=REDDIT_CLIENT_ID, client_secret=REDDIT_CLIENT_SECRET,
                          username=REDDIT_UNM, password=REDDIT_PWD, user_agent=REDDIT_USER_AGENT)
@@ -55,6 +43,7 @@ async def send_memes():
     for submission in hot_posts:
         if not submission.stickied:
             await channel.send('\"{0.title}\" - {0.author}\n{0.url}'.format(submission))
+            await asyncio.sleep(INTERVAL)
 
 
 @send_memes.before_loop
